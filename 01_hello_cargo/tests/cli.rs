c09f9c40275fd08f466a04ce851e87333e40fba9
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-21 12:43:48 -0700

    trying to get intelly sence to work

--- 01_hello_cargo/tests/cli.rs
+++ 01_hello_cargo/tests/cli.rs
@@ -10,3 +10,15 @@ fn runs() {
     let mut cmd = Command::cargo_bin("hello_cargo").unwrap();
     cmd.assert().success();
 }
+
+//#[test]
+//fn true_ok() {
+//    let mut cmd = Command::
+//    cmd.assert().success();
+//}
+
+#[test]
+fn false_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
