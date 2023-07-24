970f0343dcc07ff13fc0cb60cd0abd2a94575c29
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-24 12:05:17 -0700

    clap works

--- 02_echo/src/main.rs
+++ 02_echo/src/main.rs
@@ -1,3 +1,9 @@
+use clap::Command;
+
 fn main() {
-    println!("{:#?}", std::env::args());
+    let _matches = Command::new("echo")
+        .version("1.0")
+        .author("FallenGameR")
+        .about("Prints arguments to the standard output")
+        .get_matches();
 }
