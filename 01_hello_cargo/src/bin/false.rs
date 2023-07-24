a5a0079139cc83b3c8cf6d9ae16a7ccf91c31d6a
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-24 10:38:38 -0700

    now ti works =)

--- 01_hello_cargo/src/bin/false.rs
+++ 01_hello_cargo/src/bin/false.rs
@@ -1,4 +1,3 @@
 fn main() {
-    println!("Hello false");
     std::process::exit(1);
 }
--- 01_hello_cargo/tests/cli.rs
+++ 01_hello_cargo/tests/cli.rs
@@ -6,15 +6,27 @@ fn works() {
 }
 
 #[test]
+fn ls_ok() {
+    let mut cmd = std::process::Command::new("ls");
+    let rest = cmd.output();
+    assert!(rest.is_ok());
+}
+
+#[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("hello_cargo").unwrap();
     cmd.assert().success();
 }
 
 #[test]
-fn ls_ok() {
-    let mut cmd = std::process::Command::new("ls");
-    let rest = cmd.output();
-    assert!(rest.is_ok());
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
 
