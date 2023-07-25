58f95eb614abd6aaf5d67c6e4544717a946e92cb
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-25 12:39:43 -0700

    works

--- 02_echo/src/main.rs
+++ 02_echo/src/main.rs
@@ -1,15 +1,19 @@
 use clap::{arg, Command};
 
 fn main() {
-    let matches = Command::new("echo")
+    let mut matches = Command::new("echo")
         .version("1.0")
         .author("FallenGameR")
         .about("Prints arguments to the standard output")
         .args(&[
-            arg!(<TEXT> ... "Input text"),
+            arg!(<text> ... "Input text"),
             arg!(-n --newline "Do not print newline")
         ])
         .get_matches();
 
-    println!("{:?}", matches);
+    let text: Vec<String> = matches.remove_many("text").expect("No text provided").collect();
+    let newline = matches.get_flag("newline");
+
+    println!("{:?}", text);
+    println!("{:?}", newline);
 }
