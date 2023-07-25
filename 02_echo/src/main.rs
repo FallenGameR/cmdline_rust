9289de5e51c0b75f51344fdc7d25f3eed506487f
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-25 12:53:37 -0700

    -n doesn't work in powershell, output is always terminated in a newline. Bash however spots the difference.

--- 02_echo/src/main.rs
+++ 02_echo/src/main.rs
@@ -7,13 +7,13 @@ fn main() {
         .about("Prints arguments to the standard output")
         .args([
             arg!(<text> ... "Input text"),
-            arg!(-n --newline "Do not print newline")
+            arg!(-n --no_newline "Do not print newline")
         ])
         .get_matches();
 
     let text: Vec<String> = matches.remove_many("text").expect("No text provided").collect();
-    let newline = matches.get_flag("newline");
+    let newline = !matches.get_flag("no_newline");
 
-    println!("{:?}", text);
-    println!("{:?}", newline);
+    let ending = if newline { "\n" } else { "" };
+    print!("{}{}", text.join(" "), ending);
 }
