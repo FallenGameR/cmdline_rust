3cd046d2d00450e40f1cfca62454afb0d105a4ec
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-26 10:42:43 -0700

    hello test done

--- 02_echo/tests/cli.rs
+++ 02_echo/tests/cli.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use assert_cmd::Command;
 
 #[test]
@@ -10,4 +12,12 @@ fn dies_no_args() {
 fn runs() {
     let mut cmd = Command::cargo_bin("echo").unwrap();
     cmd.arg("Hello").assert().success();
+}
+
+#[test]
+fn hello1() {
+    let path = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(path).unwrap();
+    let mut cmd = Command::cargo_bin("echo").unwrap();
+    cmd.arg("Hello there").assert().success().stdout(expected);
 }
