84104c437b6ac424796d4166d781d4be71161e40
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-26 10:53:04 -0700

    using TestResult instead of unwrap

--- 02_echo/tests/cli.rs
+++ 02_echo/tests/cli.rs
@@ -1,23 +1,29 @@
+use assert_cmd::Command;
 use std::fs;
 
-use assert_cmd::Command;
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.assert().failure().stderr(predicates::str::contains("Usage:"));
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echo")?;
+    cmd.assert()
+        .failure()
+        .stderr(predicates::str::contains("Usage:"));
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echo").unwrap();
+fn runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("echo")?;
     cmd.arg("Hello").assert().success();
+    Ok(())
 }
 
 #[test]
-fn hello1() {
+fn hello1() -> TestResult {
     let path = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(path).unwrap();
-    let mut cmd = Command::cargo_bin("echo").unwrap();
+    let expected = fs::read_to_string(path)?;
+    let mut cmd = Command::cargo_bin("echo")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
