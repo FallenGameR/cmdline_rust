7c433b4f5b20d19bf5aa38ec978d1e87a8fa322d
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-26 11:50:18 -0700

    works =)

--- 02_echo/tests/cli.rs
+++ 02_echo/tests/cli.rs
@@ -21,14 +21,22 @@ fn runs() -> TestResult {
 
 #[test]
 fn hello1() -> TestResult {
-    run(&vec!["Hello there"], "tests/expected/hello1.txt")?;
-    Ok(())
+    run(&vec!["Hello there"], "tests/expected/hello1.txt")
 }
 
 #[test]
 fn hello2() -> TestResult {
-    run(&vec!["Hello", "there"], "tests/expected/hello2.txt")?;
-    Ok(())
+    run(&vec!["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&vec!["Hello   there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&vec!["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
 
 fn run(args: &[&str], path: &str) -> TestResult {
