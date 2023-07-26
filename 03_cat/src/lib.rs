453ad7659ec41414c77a6f066391347828660198
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-07-26 12:46:07 -0700

    files can be absent, but that would mean - plus added exclusive relationship between arguments

--- 03_cat/src/lib.rs
+++ 03_cat/src/lib.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use clap::{Command, arg};
+use clap::{arg, Command};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -11,7 +11,7 @@ pub struct Config {
     number_nonblank_lines: bool,
 }
 
-pub fn run (config: Config) -> MyResult<()> {
+pub fn run(config: Config) -> MyResult<()> {
     dbg!(&config);
     Ok(())
 }
@@ -20,17 +20,22 @@ pub fn get_args() -> MyResult<Config> {
     let mut matches = Command::new("cat")
         .version("1.0")
         .author("FallenGameR")
-        .about("Concatenates files and can add line numbers")
+        .about("Concatenates files and optionally add line numbers")
         .args([
-            arg!(<files> ... "Input files to concatenate"),
-            arg!(-n --number_lines "Add line numbers to output"),
-            arg!(-b --number_nonblank_lines "Number only nonblack lines"),
+            arg!([files] ... "Input files to concatenate, stdin if empty").default_value("-"),
+            arg!(-n --number_lines "Add line numbers to output")
+                .conflicts_with("number_nonblank_lines"),
+            arg!(-b --number_nonblank_lines "Number only nonblack lines")
+                .conflicts_with("number_lines"),
         ])
         .get_matches();
 
     Ok(Config {
-        files: matches.remove_many("files").expect("No file paths provided").collect(),
+        files: matches
+            .remove_many("files")
+            .expect("No file paths provided")
+            .collect(),
         number_lines: matches.get_flag("number_lines"),
         number_nonblank_lines: matches.get_flag("number_nonblank_lines"),
-     })
+    })
 }
