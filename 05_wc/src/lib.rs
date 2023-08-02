b7ea2f5d947013cb7bb18b1ced4844c5d23cdb4e
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-08-02 14:22:45 -0700

    importing over more tests

--- 05_wc/src/lib.rs
+++ 05_wc/src/lib.rs
@@ -1,52 +1,69 @@
-use std::{error::Error, io::{BufRead, BufReader}};
 use clap::{arg, Command};
-
-const PAGE_SIZE: usize = 4096;
-const BUFFER_SIZE: usize = PAGE_SIZE * 2;
+use std::{
+    error::Error,
+    io::{BufRead, BufReader},
+};
 
 type DynErrorResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
+    lines: bool,
+    words: bool,
+    bytes: bool,
+    chars: bool,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Stats {
     lines: usize,
-    bytes: Option<usize>,
+    words: usize,
+    bytes: usize,
+    chars: usize,
 }
 
 pub fn get_args() -> DynErrorResult<Config> {
     let mut matches = Command::new("head")
         .version("1.0")
         .author("FallenGameR")
-        .about("Prints begining of files for a preview")
+        .about("Calculates string statistic from files: lines, words, chars, bytes")
         .args([
-            arg!([FILES] ... "Files to preview, stdin is -")
-                .default_value("-"),
-            arg!(-n --lines <LINES> "Number of lines to show")
-                .value_parser(clap::value_parser!(usize))
-                .default_value("10")
-                .conflicts_with("bytes"),
-            arg!(-c --bytes <BYTES> "Number of bytes to show")
-                .value_parser(clap::value_parser!(usize))
-                .conflicts_with("lines"),
+            arg!([FILES] ... "Files to process, stdin is -").default_value("-"),
+            arg!(-l --lines "Count lines as number of EOL sequences"),
+            arg!(-w --words "Count words as strings separated by whitespace"),
+            arg!(-c --chars "Count number of characters"),
+            arg!(-b --bytes "Count number of bytes"),
         ])
         .get_matches();
 
-    Ok(Config {
+    let mut config = Config {
         files: matches
             .remove_many("FILES")
             .expect("No file paths provided")
             .collect(),
-        lines: matches
-            .remove_one("lines")
-            .expect("No number of lines provided"),
-        bytes: matches
-            .remove_one("bytes"),
-    })
+        lines: matches.get_flag("lines"),
+        words: matches.get_flag("words"),
+        chars: matches.get_flag("chars"),
+        bytes: matches.get_flag("bytes"),
+    };
+
+    // If no flags are provided, default to all flags are on
+    if !config.lines && !config.words && !config.chars && !config.bytes {
+        config.lines = true;
+        config.words = true;
+        config.chars = true;
+        config.bytes = true;
+    }
+
+    Ok(config)
 }
 
 // cargo run -- -n (ls .\tests\inputs\*.txt)
 // cargo run -- -n (walker .\tests\inputs\ -a)
 pub fn run(config: Config) -> DynErrorResult<()> {
+    println!("{:?}", config);
+
     let mutifile_handling = config.files.len() > 1;
     let mut entry_separator_needed = false;
 
@@ -73,11 +90,12 @@ pub fn run(config: Config) -> DynErrorResult<()> {
 fn open(path: &str) -> DynErrorResult<Box<dyn BufRead>> {
     match path {
         "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
-        _ => Ok(Box::new(BufReader::new(std::fs::File::open(path)?)))
+        _ => Ok(Box::new(BufReader::new(std::fs::File::open(path)?))),
     }
 }
 
 fn process_file(path: &str, mut reader: Box<dyn BufRead>, config: &Config) {
+    /*
     let buffer_size = config.bytes.unwrap_or(BUFFER_SIZE);
     let mut buffer = vec![0; buffer_size];
 
@@ -88,9 +106,11 @@ fn process_file(path: &str, mut reader: Box<dyn BufRead>, config: &Config) {
         Ok(len) => process_buffer(&buffer[0..len], config),
         Err(error) => eprintln!("Can't open file '{}', error {}", path, error),
     }
+    */
 }
 
-fn process_buffer(buffer: &[u8], config: &Config) {
+fn process_stats(mut file: impl BufRead) -> DynErrorResult<Stats> {
+    /*
     let text = String::from_utf8_lossy(buffer);
     let mut line_count = 0;
 
@@ -104,5 +124,48 @@ fn process_buffer(buffer: &[u8], config: &Config) {
         if line_count >= config.lines {
             break;
         }
+    }
+    */
+
+    Ok(Stats {
+        lines: 0,
+        words: 0,
+        bytes: 0,
+        chars: 0,
+    })
+}
+
+fn format_field(value: usize, show: bool) -> String {
+    if show {
+        format!("{:>8}", value)
+    } else {
+        "".to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{process_stats, format_field, Stats};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your half.\r\n";
+        let info = process_stats(Cursor::new(text));
+        assert!(info.is_ok());
+        let expected = Stats {
+            lines: 1,
+            words: 10,
+            chars: 48,
+            bytes: 48,
+        };
+        assert_eq!(info.unwrap(), expected);
+    }
+
+    #[test]
+    fn test_format_field() {
+        assert_eq!(format_field(1, false), "");
+        assert_eq!(format_field(3, true), "       3");
+        assert_eq!(format_field(10, true), "      10");
     }
 }
