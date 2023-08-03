78daea1fb1b4fbebc3835810fec5a1c322ed9c56
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-08-03 12:39:22 -0700

    draft implementation

--- 05_wc/src/lib.rs
+++ 05_wc/src/lib.rs
@@ -23,13 +23,22 @@ pub struct Stats {
     chars: usize,
 }
 
+impl std::ops::AddAssign for Stats {
+    fn add_assign(&mut self, other: Stats) {
+        self.lines += other.lines;
+        self.words += other.words;
+        self.chars += other.chars;
+        self.bytes += other.bytes;
+    }
+}
+
 pub fn get_args() -> DynErrorResult<Config> {
     let mut matches = Command::new("head")
         .version("1.0")
         .author("FallenGameR")
         .about("Calculates string statistic from files: lines, words, chars, bytes")
         .args([
-            arg!([FILES] ... "Files to process, stdin is -").default_value("-"),
+            arg!([files] ... "Files to process, stdin is -").default_value("-"),
             arg!(-l --lines "Count lines as number of EOL sequences"),
             arg!(-w --words "Count words as strings separated by whitespace"),
             arg!(-c --chars "Count number of characters"),
@@ -39,7 +48,7 @@ pub fn get_args() -> DynErrorResult<Config> {
 
     let mut config = Config {
         files: matches
-            .remove_many("FILES")
+            .remove_many("files")
             .expect("No file paths provided")
             .collect(),
         lines: matches.get_flag("lines"),
@@ -62,67 +71,60 @@ pub fn get_args() -> DynErrorResult<Config> {
 // cargo run -- -n (ls .\tests\inputs\*.txt)
 // cargo run -- -n (walker .\tests\inputs\ -a)
 pub fn run(config: Config) -> DynErrorResult<()> {
-    println!("{:?}", config);
-
-    let mutifile_handling = config.files.len() > 1;
-    let mut entry_separator_needed = false;
+    let mut totals = Stats { lines: 0, words: 0, bytes: 0, chars: 0 };
+    let mut files_processed = 0;
 
     for path in &config.files {
-        if entry_separator_needed {
-            println!();
-        }
-
-        entry_separator_needed = true;
-
-        if mutifile_handling {
-            println!("==> {} <==", path);
-        }
-
         match open(&path) {
-            Ok(reader) => process_file(&path, reader, &config),
             Err(error) => eprintln!("Can't open file '{}', error {}", &path, error),
+            Ok(reader) =>
+            {
+                let stats = process_stats(reader)?;
+                output_stats(&stats, &path, &config);
+                files_processed += 1;
+                totals += stats;
+            },
         }
+    }
+
+    if files_processed > 1 {
+        output_stats(&totals, "total", &config);
     }
 
     Ok(())
 }
 
-fn open(path: &str) -> DynErrorResult<Box<dyn BufRead>> {
-    match path {
-        "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
-        _ => Ok(Box::new(BufReader::new(std::fs::File::open(path)?))),
-    }
-}
-
-
-fn process_file(path: &str, mut reader: Box<dyn BufRead>, config: &Config) {
-    /*
-    let buffer_size = config.bytes.unwrap_or(BUFFER_SIZE);
-    let mut buffer = vec![0; buffer_size];
-
-    // Alternatively we could do the following:
-    // reader.bytes().take(buffer_size as u64);
-
-    match reader.read(&mut buffer) {
-        Ok(len) => process_buffer(&buffer[0..len], config),
-        Err(error) => eprintln!("Can't open file '{}', error {}", path, error),
-    }
-    */
-}
-
-fn process_stats(reader: impl BufRead) -> DynErrorResult<Stats> {
+fn process_stats(mut reader: impl BufRead) -> DynErrorResult<Stats> {
     let mut result = Stats { lines: 0, words: 0, bytes: 0, chars: 0 };
+    let mut line = String::new();
 
-    for line in reader.lines() {
-        let line = line?;
+    loop {
+        let bytes = reader.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
 
-        result.bytes += line.len();
+        result.bytes += bytes;
         result.chars += line.chars().count();
         result.words += line.split_whitespace().count();
         result.lines += 1;
+
+        line.clear();
     }
 
     Ok(result)
+}
+
+fn output_stats(stats: &Stats, name: &str, config: &Config)
+{
+    println!(
+        "{}{}{}{}{}",
+        format_field(stats.lines, config.lines),
+        format_field(stats.words, config.words),
+        format_field(stats.chars, config.chars),
+        format_field(stats.bytes, config.bytes),
+        name
+    );
 }
 
 fn format_field(value: usize, show: bool) -> String {
@@ -130,6 +132,13 @@ fn format_field(value: usize, show: bool) -> String {
         format!("{:>8}", value)
     } else {
         "".to_string()
+    }
+}
+
+fn open(path: &str) -> DynErrorResult<Box<dyn BufRead>> {
+    match path {
+        "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(std::fs::File::open(path)?))),
     }
 }
 
