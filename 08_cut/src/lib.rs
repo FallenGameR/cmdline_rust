6c6b5c251a1e221d8b787fd031043e214cf0a270
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-08-16 10:38:45 -0700

    preparing for the parsing

--- 08_cut/src/lib.rs
+++ 08_cut/src/lib.rs
@@ -1,31 +1,40 @@
-use std::{error::Error, io::{BufRead, BufReader}};
+use std::{error::Error, io::{BufRead, BufReader}, ops::Range};
 use clap::{arg, Command};
 
 const PAGE_SIZE: usize = 4096;
 const BUFFER_SIZE: usize = PAGE_SIZE * 2;
 
 type DynErrorResult<T> = Result<T, Box<dyn Error>>;
+type Positions = Vec<Range<usize>>;
+
+#[derive(Debug)]
+pub enum ExtractedRanges
+{
+    Bytes(Positions),
+    Chars(Positions),
+    Fields(Positions),
+}
 
 #[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
-    lines: usize,
-    bytes: Option<usize>,
+    delimeter: u8,              // can be only an ASCII char
+    extracted: ExtractedRanges,
 }
 
 pub fn get_args() -> DynErrorResult<Config> {
-    let mut matches = Command::new("head")
+    let mut matches = Command::new("cut")
         .version("1.0")
         .author("FallenGameR")
-        .about("Prints begining of files for a preview")
+        .about("Extracts data from a text file")
         .args([
-            arg!([FILES] ... "Files to preview, stdin is -")
+            arg!([FILES] ... "Files to process, stdin is -")
                 .default_value("-"),
-            arg!(-n --lines <LINES> "Number of lines to show")
-                .value_parser(clap::value_parser!(usize))
+            arg!(-d --delimeter "Delimeter to use, tab is default")
                 .default_value("10")
+                .value_parser(clap::value_parser!(usize))
                 .conflicts_with("bytes"),
-            arg!(-c --bytes <BYTES> "Number of bytes to show")
+            arg!(-b --bytes <BYTES> "What byte ranges to extract, e.g. 1, 3-5, 2")
                 .value_parser(clap::value_parser!(usize))
                 .conflicts_with("lines"),
         ])
@@ -37,16 +46,21 @@ pub fn get_args() -> DynErrorResult<Config> {
             .expect("No file paths provided")
             .collect(),
         lines: matches
-            .remove_one("lines")
+            .remove_many("lines")
             .expect("No number of lines provided"),
         bytes: matches
             .remove_one("bytes"),
     })
 }
 
-// cargo run -- -n (ls .\tests\inputs\*.txt)
-// cargo run -- -n (walker .\tests\inputs\ -a)
+fn parse_positions(range: &str) -> DynErrorResult<Positions> {
+    unimplemented!()
+}
+
 pub fn run(config: Config) -> DynErrorResult<()> {
+    println!("{:?}", config);
+    Ok(())
+    /*
     let mutifile_handling = config.files.len() > 1;
     let mut entry_separator_needed = false;
 
@@ -68,6 +82,7 @@ pub fn run(config: Config) -> DynErrorResult<()> {
     }
 
     Ok(())
+    */
 }
 
 fn open(path: &str) -> DynErrorResult<Box<dyn BufRead>> {
@@ -78,6 +93,8 @@ fn open(path: &str) -> DynErrorResult<Box<dyn BufRead>> {
 }
 
 fn process_file(path: &str, mut reader: Box<dyn BufRead>, config: &Config) {
+    unimplemented!()
+    /*
     let buffer_size = config.bytes.unwrap_or(BUFFER_SIZE);
     let mut buffer = vec![0; buffer_size];
 
@@ -88,9 +105,12 @@ fn process_file(path: &str, mut reader: Box<dyn BufRead>, config: &Config) {
         Ok(len) => process_buffer(&buffer[0..len], config),
         Err(error) => eprintln!("Can't open file '{}', error {}", path, error),
     }
+    */
 }
 
 fn process_buffer(buffer: &[u8], config: &Config) {
+    unimplemented!()
+    /*
     let text = String::from_utf8_lossy(buffer);
     let mut line_count = 0;
 
@@ -105,4 +125,5 @@ fn process_buffer(buffer: &[u8], config: &Config) {
             break;
         }
     }
+    */
 }
