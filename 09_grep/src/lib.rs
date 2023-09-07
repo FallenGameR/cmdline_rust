b9a7118cf692814ab32932157aeb331137d05d20
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-09-07 10:35:09 -0700

    argument parsing

--- 09_grep/src/lib.rs
+++ 09_grep/src/lib.rs
@@ -1,32 +1,23 @@
 use anyhow::{bail, Result};
 use clap::{arg, Command};
-use std::{
-    io::{BufRead, BufReader},
-    num::NonZeroUsize,
-    ops::RangeInclusive,
-};
-
-type Positions = Vec<RangeInclusive<usize>>;
-
-#[derive(Debug)]
-pub enum ExtractedRanges {
-    Bytes(Positions),
-    Chars(Positions),
-    Fields(Positions),
-}
+use std::io::{BufRead, BufReader};
 
 #[derive(Debug)]
 pub struct Config {
+    pattern: String,
     files: Vec<String>,
-    extracted: ExtractedRanges,
-    delimeter: char,
+    recurse: bool,
+    count: bool,
+    invert: bool,
 }
 
 pub fn run(config: Config) -> Result<()> {
+    dbg!(&config);
+
     for path in &config.files {
         match open(path) {
-            Ok(reader) => process_file(path, reader, &config)?,
             Err(error) => eprintln!("Can't open file '{}', error {}", &path, error),
+            Ok(reader) => process_file(path, reader, &config)?,
         }
     }
 
@@ -42,99 +33,33 @@ fn open(path: &str) -> Result<Box<dyn BufRead>> {
 
 pub fn get_args() -> Result<Config> {
     // CLI arguments
-    let mut matches = Command::new("cut")
+    let mut matches = Command::new("grep")
         .version("1.0")
         .author("FallenGameR")
-        .about("Extracts data from a text file")
+        .about("Finds text specified by regular expression in files")
         .args([
-            arg!([FILES] ... "Files to process, stdin is -").default_value("-"),
-            arg!(-b --bytes <BYTES> "What byte ranges to extract, e.g. 1, 3-5")
-                .value_parser(parse_ranges)
-                .conflicts_with_all(["chars", "fields"]),
-            arg!(-c --chars <CHARS> "What char ranges to extract, e.g. 5-1, 2")
-                .value_parser(parse_ranges)
-                .conflicts_with_all(["bytes", "fields"]),
-            arg!(-f --fields <FIELDS> "What field ranges to extract, e.g. 1, 3")
-                .value_parser(parse_ranges)
-                .conflicts_with_all(["bytes", "chars"]),
-            arg!(-d --delimeter <DELIMETER> "Fields delimeter, tab is default")
-                .value_parser(clap::value_parser!(char))
-                .default_value("\t"),
+            arg!(-e --expression <REGULAR_EXPRESSION> "Regular expression to use")
+                .default_value("."),
+            arg!([FILES] ... "Files or folders to process, stdin is -").default_value("-"),
+            arg!(-r --recurse "Recuresivelly descend into folders looking for files"),
+            arg!(-c --count "Just count the matches, don't show them"),
+            arg!(-i --invert "Find lines that don't match the regular expression"),
         ])
         .get_matches();
 
-    // Make sure that only one range type is provided
-    let mut selected_count = 0;
-    let mut selected = "";
-
-    if matches.contains_id("bytes") {
-        selected_count += 1;
-        selected = "bytes";
-    }
-
-    if matches.contains_id("chars") {
-        selected_count += 1;
-        selected = "chars";
-    }
-
-    if matches.contains_id("fields") {
-        selected_count += 1;
-        selected = "fields";
-    }
-
-    if selected_count != 1 {
-        bail!("Please provide either --bytes --chars or --fields once");
-    }
-
-    // Composing the config
+    // Construct config
     Ok(Config {
+        pattern: matches
+            .remove_one("expression")
+            .expect("No pattern provided"),
         files: matches
             .remove_many("FILES")
             .expect("No file paths provided")
             .collect(),
-        extracted: {
-            let ranges = matches
-                .remove_one(selected)
-                .expect("No ranges were provided");
-
-            match selected {
-                "bytes" => ExtractedRanges::Bytes(ranges),
-                "chars" => ExtractedRanges::Chars(ranges),
-                "fields" => ExtractedRanges::Fields(ranges),
-                _ => unreachable!("Unknown range type"),
-            }
-        },
-        delimeter: matches
-            .remove_one("delimeter")
-            .expect("No delimeter was provided"),
+        recurse: matches.get_flag("recurse"),
+        count: matches.get_flag("count"),
+        invert: matches.get_flag("invert"),
     })
-}
-
-fn parse_ranges(ranges: &str) -> Result<Vec<RangeInclusive<usize>>> {
-    ranges.split(',').map(|x| parse_range(x.trim())).collect()
-}
-
-fn parse_range(range: &str) -> Result<RangeInclusive<usize>> {
-    let result: Result<Vec<NonZeroUsize>, _> = range.split('-').map(str::parse).collect();
-
-    // Input: inclusive range as indexes, positive indexes
-    // Output: inclusive range as range, zero-based indexes
-    let construct = |start, end| -> Result<RangeInclusive<usize>> {
-        Ok(usize::from(start) - 1..=usize::from(end) - 1)
-    };
-
-    match result {
-        Err(error) => bail!("Invalid range '{}' - {}", range, error),
-        Ok(parts) => match parts.len() {
-            1 => construct(parts[0], parts[0]),
-            2 => construct(parts[0], parts[1]),
-            _ => bail!(
-                "Invalid range '{}' - wrong number of range parts {}",
-                range,
-                parts.len()
-            ),
-        },
-    }
 }
 
 fn process_file(path: &str, reader: Box<dyn BufRead>, _: &Config) -> Result<()> {
--- 09_grep/src/main.rs
+++ 09_grep/src/main.rs
@@ -1,43 +1,6 @@
 fn main() {
-    fn main() {
-        if let Err(error) = grep::get_args().and_then(grep::run) {
-            eprintln!("{error}");
-            std::process::exit(1);
-        }
+    if let Err(error) = grep::get_args().and_then(grep::run) {
+        eprintln!("{error}");
+        std::process::exit(1);
     }
-
-
-    dbg!("Hello, world!"); // can return stuff
-    // A Pin<P> ensures that the pointee of any pointer type P has a stable location in memory (self-referencing struct)
-    // println!("Initializing an instance of {}", std::any::type_name::<T>());
-
-    // let bar = Some(4);
-    // assert!(matches!(bar, Some(x) if x > 2))
-
-    // Subslice patterns
-    // fn foo(words: &[&str]) {
-    //     match words {
-    //         // Ignore everything but the last element, which must be "!".
-    //         [.., "!"] => println!("!!!"),
-    //
-    //         // `start` is a slice of everything except the last element, which must be "z".
-    //         [start @ .., "z"] => println!("starts with: {:?}", start),
-    //
-    //         // `end` is a slice of everything but the first element, which must be "a".
-    //         ["a", end @ ..] => println!("ends with: {:?}", end),
-    //
-    //         rest => println!("{:?}", rest),
-    //     }
-    // }
-
-    // let exe = env!("PATH");
-
-    // match x as u32 {
-    //     0 => println!("zero!"),
-    //     1.. => println!("positive number!"),
-    // }
-
-    // cargo build --timings
-
-    // let else blocks
 }
