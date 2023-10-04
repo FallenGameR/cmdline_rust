9e3f1588a2df4195ed5ac76564315d0d512cc38c
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-10-04 13:01:10 -0700

    figured out that -h flag is for a different thing

--- 14_ls/src/lib.rs
+++ 14_ls/src/lib.rs
@@ -16,14 +16,12 @@ pub fn get_args() -> Result<Config> {
     let mut matches = Command::new("ls")
         .version("1.0")
         .author("FallenGameR")
-        .about("List file system entries")
+        .about("List file system entries on Linux")
         .args([
-            arg!([PATHS] ... "Paths to process, current folder is -").default_value("."),
+            arg!([PATHS] ... "Paths to process, current folder is .").default_value("."),
             arg!(-l --long "Use long format that shows each entry per line"),
-            arg!(-h --hidden "Show hidden file system entries"),
-            arg!(-H --help "Print help").action(ArgAction::Help),
+            arg!(-a --all "Show all file system entries, including hidden ones"),
         ])
-        .disable_help_flag(true)
         .get_matches();
 
     Ok(Config {
@@ -32,7 +30,7 @@ pub fn get_args() -> Result<Config> {
             .expect("No paths provided")
             .collect(),
         use_long_format: matches.get_flag("long"),
-        show_hidden: matches.get_flag("hidden"),
+        show_hidden: matches.get_flag("all"),
     })
 }
 
