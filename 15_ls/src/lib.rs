a0834c3ab2a388654bb5648014590ee50baa46bc
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-10-04 10:38:07 -0700

    imported over head program

---
+++ 15_ls/src/lib.rs
@@ -0,0 +1,108 @@
+use std::{error::Error, io::{BufRead, BufReader}};
+use clap::{arg, Command};
+
+const PAGE_SIZE: usize = 4096;
+const BUFFER_SIZE: usize = PAGE_SIZE * 2;
+
+type DynErrorResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> DynErrorResult<Config> {
+    let mut matches = Command::new("head")
+        .version("1.0")
+        .author("FallenGameR")
+        .about("Prints begining of files for a preview")
+        .args([
+            arg!([FILES] ... "Files to preview, stdin is -")
+                .default_value("-"),
+            arg!(-n --lines <LINES> "Number of lines to show")
+                .value_parser(clap::value_parser!(usize))
+                .default_value("10")
+                .conflicts_with("bytes"),
+            arg!(-c --bytes <BYTES> "Number of bytes to show")
+                .value_parser(clap::value_parser!(usize))
+                .conflicts_with("lines"),
+        ])
+        .get_matches();
+
+    Ok(Config {
+        files: matches
+            .remove_many("FILES")
+            .expect("No file paths provided")
+            .collect(),
+        lines: matches
+            .remove_one("lines")
+            .expect("No number of lines provided"),
+        bytes: matches
+            .remove_one("bytes"),
+    })
+}
+
+// cargo run -- -n (ls .\tests\inputs\*.txt)
+// cargo run -- -n (walker .\tests\inputs\ -a)
+pub fn run(config: Config) -> DynErrorResult<()> {
+    let mutifile_handling = config.files.len() > 1;
+    let mut entry_separator_needed = false;
+
+    for path in &config.files {
+        if entry_separator_needed {
+            println!();
+        }
+
+        entry_separator_needed = true;
+
+        if mutifile_handling {
+            println!("==> {path} <==");
+        }
+
+        match open(path) {
+            Ok(reader) => process_file(path, reader, &config),
+            Err(error) => eprintln!("Can't open file '{}', error {}", &path, error),
+        }
+    }
+
+    Ok(())
+}
+
+fn open(path: &str) -> DynErrorResult<Box<dyn BufRead>> {
+    match path {
+        "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(std::fs::File::open(path)?)))
+    }
+}
+
+fn process_file(path: &str, mut reader: Box<dyn BufRead>, config: &Config) {
+    let buffer_size = config.bytes.unwrap_or(BUFFER_SIZE);
+    let mut buffer = vec![0; buffer_size];
+
+    // Alternatively we could do the following:
+    // reader.bytes().take(buffer_size as u64);
+
+    match reader.read(&mut buffer) {
+        Ok(len) => process_buffer(&buffer[0..len], config),
+        Err(error) => eprintln!("Can't open file '{path}', error {error}"),
+    }
+}
+
+fn process_buffer(buffer: &[u8], config: &Config) {
+    let text = String::from_utf8_lossy(buffer);
+    let mut line_count = 0;
+
+    for char in text.chars() {
+        print!("{char}");
+
+        if char == '\n' {
+            line_count += 1;
+        }
+
+        if line_count >= config.lines {
+            break;
+        }
+    }
+}
